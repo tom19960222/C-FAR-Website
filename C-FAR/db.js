c40a768c17d54f4ebb71d6e9aafb0bff086bffde
爆頭專家 <tom19960222@gmail.com>
2016-01-12 16:29:32 +0800

    Add database port in the config file.

--- C-FAR/db.js
+++ C-FAR/db.js
@@ -2,7 +2,7 @@ var Sequelize = require('sequelize');
 var sequelize = new Sequelize('c-far', 'root', '123456@', {
   host: '192.168.99.100',
   dialect: 'mysql',
-
+  port: 3306,
   pool: {
     max: 5,
     min: 0,
