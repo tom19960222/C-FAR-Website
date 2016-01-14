3f5d5401d04f6fde7e6dbc94b49d8199c5a2e45b
Len_AshBell <jason841022@yahoo.com.tw>
2016-01-14 14:09:43 +0800

    增加 form 功能，爽爽der~XDD

--- C-FAR/main/public/assets/manage/js/form-getData.js
+++ C-FAR/main/public/assets/manage/js/form-getData.js
@@ -4,5 +4,22 @@
 
 
 var dataType = [
+    ""
+];
 
-]
+
+var Container = $('.container');
+
+function CreateTitle(data){
+    var title = '<div class="row title"><div class="col-12 font-thin">' + data + '</div></div><!--title-row-->';
+    Container.append(title);
+}
+
+function CreateSubtitle(data){
+    var title = '<div class="row subtitle"><div class="col-12 font-thin">' + data + '</div></div><!--subtitle-row-->';
+    Container.append(title);
+}
+
+
+CreateTitle('XDDD');
+CreateSubtitle('QAQ');
