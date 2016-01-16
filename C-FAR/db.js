cb8ae739208dd72f6ba4472a53f6d07e76ec35e6
爆頭專家 <tom19960222@gmail.com>
2016-01-16 23:00:40 +0800

    Fix wrong parameter name.

--- C-FAR/db.js
+++ C-FAR/db.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 var config = require('./config');
-var sequelize = new Sequelize(config.database.name, config.database.username, config.database.password, {
+var sequelize = new Sequelize(config.database.dbname, config.database.username, config.database.password, {
   host: config.database.host,
   dialect: config.database.type,
   port: config.database.port,
