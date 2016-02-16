4429a70e2fcd9bebd7851c7f3abdf8a69044d1e6
爆頭專家 <tom19960222@gmail.com>
2016-02-16 09:06:23 +0800

    article操作改用transaction

--- C-FAR/article/action.js
+++ C-FAR/article/action.js
@@ -1,22 +1,33 @@
 var Article = require('./models').Article;
 var User = require('../user/models').User;
+var sequelize = require('../db');
 
 var addArticle = function(title, summary, content, cover_image_url, creator, required_permission, visable){
-    return Article.create({
-        title: title,
-        summary: summary,
-        content: content,
-        cover_image_url: cover_image_url,
-        required_permission: required_permission,
-        visable: visable
-    })
-    .then(function(_article){
-          return User.findById(creator)
-          .then(function(_user){
-              _user.addArticle(_article);
-              return _article.setCreator(_user);
-          })
-    })
+    var article, user;
+    return sequelize.transaction(function(t){
+        return Article.create({
+            title: title,
+            summary: summary,
+            content: content,
+            cover_image_url: cover_image_url,
+            required_permission: required_permission,
+            visable: visable
+        }, {transaction: t})
+        .then(function(_article){
+            article = _article;
+            return User.findById(creator)
+        }) 
+        .then(function(_user){
+            user = _user;
+            return user.addArticle(article, {transaction: t});
+        })
+        .then(function(){
+            return article.setCreator(user, {transaction: t});
+        })
+        .catch(function(err){
+            console.error('Add article error!', err);
+        })
+    }) 
 };
 
 var getArticle = function(id){
@@ -25,26 +36,30 @@ var getArticle = function(id){
 
 var editArticle = function(id, title, summary, content, cover_image_url, required_permission, visable){
     var tmpArticle;
-    return Article.findById(id)
-    .then(function(_article){
-        tmpArticle = _article;
-        
-        if(title) tmpArticle.title = title;
-        if(summary) tmpArticle.summary = summary;
-        if(content) tmpArticle.content = content;
-        if(cover_image_url) tmpArticle.cover_image_url = cover_image_url;
-        if(required_permission) tmpArticle.required_permission = required_permission;
-        if(visable != undefined) tmpArticle.visable = visable;
-        
-        return tmpArticle.save();
-    })
+    return sequelize.transaction(function(t){
+        return Article.findById(id)
+        .then(function(_article){
+            tmpArticle = _article;
+            
+            if(title) tmpArticle.title = title;
+            if(summary) tmpArticle.summary = summary;
+            if(content) tmpArticle.content = content;
+            if(cover_image_url) tmpArticle.cover_image_url = cover_image_url;
+            if(required_permission) tmpArticle.required_permission = required_permission;
+            if(visable != undefined) tmpArticle.visable = visable;
+            
+            return tmpArticle.save({transaction: t});
+        }) 
+    }) 
 }
 
 var deleteArticle = function(id){
-    return Article.findById(id)
-    .then(function(_article){
-        return _article.destroy();
-    })
+    return sequelize.transaction(function(t){
+       return Article.findById(id)
+        .then(function(_article){
+            return _article.destroy({transaction: t});
+        }) 
+    }) 
 }
 
 module.exports.addArticle = addArticle;
