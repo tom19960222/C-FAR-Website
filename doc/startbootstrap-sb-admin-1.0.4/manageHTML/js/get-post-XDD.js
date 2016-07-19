361a9f1934191fbbd75ab19e10c1d2b7788c9553
Len_AshBell <jason841022@yahoo.com.tw>
2016-07-19 12:47:53 +0800

    update ajaxReq

--- doc/startbootstrap-sb-admin-1.0.4/manageHTML/js/get-post-XDD.js
+++ doc/startbootstrap-sb-admin-1.0.4/manageHTML/js/get-post-XDD.js
@@ -1,3 +1,76 @@
+
+
+
+var fn_list = {
+
+	user_login: 	{url: "http://cfar.hsexpert.net/user/login", method: "POST"},
+	user_add: 		{url: "http://cfar.hsexpert.net/user/add", method: "POST"},
+
+	image_get: 		{url: "http://cfar.hsexpert.net/image", method: "GET"},
+	image_add: 		{url: "http://cfar.hsexpert.net/image", method: "POST"},
+	image_update: 	{url: "http://cfar.hsexpert.net/image", method: "PUT"},
+
+	member_get: 	{url: "http://cfar.hsexpert.net/member", method: "GET"},
+	member_add: 	{url: "http://cfar.hsexpert.net/member", method: "POST"},
+	member_edit: 	{url: "http://cfar.hsexpert.net/member", method: "PUT"},
+	member_delete: 	{url: "http://cfar.hsexpert.net/member", method: "DELETE"},
+
+	message_get: 	{url: "http://cfar.hsexpert.net/message", method: "GET"},
+	message_add: 	{url: "http://cfar.hsexpert.net/message", method: "POST"},
+	message_edit: 	{url: "http://cfar.hsexpert.net/message", method: "PUT"},
+	message_delete: {url: "http://cfar.hsexpert.net/message", method: "DELETE"},
+
+	article_get: 	{url: "http://cfar.hsexpert.net/article", method: "GET"},
+	article_add: 	{url: "http://cfar.hsexpert.net/article", method: "POST"},
+	article_edit: 	{url: "http://cfar.hsexpert.net/article", method: "PUT"},
+	article_delete: {url: "http://cfar.hsexpert.net/article", method: "DELETE"},
+
+	ajaxReq: function (doFn, data) {
+		$.ajax({
+			method: doFn.method,
+			url: doFn.url,
+			data: data,
+			
+			success: function (data, status) {
+
+				switch(doFn){
+					case fn_list.image_get:
+						data.forEach(function(element, index, array) {
+							element.path = "http://cfar.hsexpert.net" + element.path;
+						})
+						a = data;
+		    			initAlbum();	
+	    			break;
+
+	    			case fn_list.member_get:
+	    				data.forEach(function(element, index, array) {
+	    					element.head_pic_url = "http://cfar.hsexpert.net" + element.head_pic_url;
+	    				})
+
+	    				c = data;
+	    				initMember();
+	    			break;
+
+	    			case fn_list.message_get:
+	    				b = data;
+	    			break;
+				}
+				
+			},
+			error: function (jqXHR, textStatus, errorMessage) {
+				console.log(errorMessage);
+			}
+		})
+	}
+};
+
+
+
+
+
+
+
+
 var data_album = [
 	{
 		image_id: 1,
@@ -22,12 +95,11 @@ function postADD_album() {
 		alert("請上傳一張照片");
 	else{
 		postJSON.push({
-			image_id: data_album[data_album.length-1].image_id + 1,
 			filename: $('#imgFile')[0].value.replace(/.*[\/\\]/, ''),
 			content: target.src
 		});
 
-		alert(JSON.stringify(postJSON));
+		WriteToFile(JSON.stringify(postJSON));
 	}
 
 }
@@ -48,3 +120,17 @@ function getData_album(){
 }
 
 
+//寫檔案
+ function WriteToFile(text) {
+   
+    var fso = new ActiveXObject('Scripting.FileSystemObject');
+
+    var file='img_add_test.txt';
+    var folder ='F:\\work\\HTMLpackage\\C-FAR-Website\\doc\\startbootstrap-sb-admin-1.0.4\\manageHTML\\js\\';
+    var f=folder+file;
+
+    var s = fso.CreateTextFile(f, true);
+    s.WriteLine("<?xml version='1.0' encoding='utf-8' ?>");
+    s.WriteLine(text);
+    s.Close();
+ }
