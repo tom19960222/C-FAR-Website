141187fdba9251bb8ad964f1a39e3de3d555fb98
施俊羽 <tom19960222@gmail.com>
2018-07-04 02:16:15 +0800

    Finish member update

--- modules/member/route.ts
+++ modules/member/route.ts
@@ -2,6 +2,7 @@ import * as Express from 'express';
 import * as bodyParser from 'body-parser';
 import * as action from './action';
 import * as Promise from 'bluebird';
+import * as _ from 'lodash';
 import {needLogin} from '../user';
 import {app} from "../../app";
 export let router = Express.Router();
@@ -18,8 +19,8 @@ router.get('/', (req, res) => {
     })
 });
 
-router.post('/', needLogin, jsonParser, (req, res) => {
-    return action.addMember(req.user.user_id, {
+router.post('/', jsonParser, (req, res) => {
+    return action.addMember(1, {
         ch_name: req.body.ch_name,
         job_title: req.body.job_title,
         en_name: req.body.en_name,
@@ -37,23 +38,44 @@ router.post('/', needLogin, jsonParser, (req, res) => {
     })
 })
 
-router.put('/', jsonParser, (req, res) => {
-    return action.bulkUpdateMember(req.body)
-    .then((member) => {
-        return action.getMemberList();
-    })
-    .then((memberList) => {
-        return res.status(200).json(memberList);
-    })
-    .catch((err: Error) => {
+router.put('/', jsonParser, async (req, res) => {
+    // 因為前端只肯多加一個 `order` 欄位，所以我們只好自己把全部的 `member` 抓出來，把要修改的那一項先拉掉，
+    // 再插到要的位置，再把整個順序更新，最後把除了要更新內容的那一項以外都只更新 `order`
+    // 如果以後還想再維護這專案的話，就把這坨屎重構吧
+    const updateTarget = _.get(_.compact(_.castArray(req.body)), '[0]');
+    updateTarget.member_id = parseInt(updateTarget.member_id);
+    updateTarget.order = parseInt(updateTarget.order);
+    let allMemberList = await action.getMemberList();
+    const existedMemberIndex = _.findIndex(allMemberList, member => member.member_id === updateTarget.member_id);
+    _.unset(allMemberList, `[${existedMemberIndex}]`);
+    allMemberList = _.compact(allMemberList);
+    allMemberList.splice(updateTarget.order - 1, 0, updateTarget);
+    let newMemberList = [];
+    for(const i in allMemberList){
+        allMemberList[i].order = i;
+        console.log(allMemberList[i].member_id, updateTarget.member_id);
+        if(allMemberList[i].member_id !== updateTarget.member_id)    
+            newMemberList.push(_.pick(allMemberList[i], ['member_id', 'order']));
+        else {
+            updateTarget.order = i;
+            newMemberList.push(updateTarget);
+        }
+            
+    }
+    await action.bulkUpdateMember(newMemberList);
+
+    try {
+        const updatedMemberList = await action.getMemberList();
+        return res.status(200).json(updatedMemberList);
+    }
+    catch(err) {
         if(err.message == "Member not found!")
             res.status(404).end();
         else{
             console.error(err);
             res.status(500).end();
         }
-            
-    })
+    }
 })
 
 router.delete('/', jsonParser, (req, res) => {
